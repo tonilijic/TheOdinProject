4ba715158071bdc4b7aec673f897d8d284873d70
Toni Lijic <hello.tonilijic@gmail.com>
2024-06-22 19:58:37 +0200

    update

--- Projects/Restaurant Page/src/index.js
+++ Projects/Restaurant Page/src/index.js
@@ -1,1 +1,15 @@
-import "./index.css";
+import "output.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const newTaskButton = document.querySelector("[data-add-new-task]");
+  const modal = document.querySelector("[data-modal]");
+  const closeModalButton = document.querySelector("[data-close-modal]");
+
+  newTaskButton.addEventListener("click", () => {
+    modal.showModal();
+  });
+
+  closeModalButton.addEventListener("click", () => {
+    modal.close();
+  });
+});
